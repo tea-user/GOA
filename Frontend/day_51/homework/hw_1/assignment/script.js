abe9f9c55cb9010bb0ff0f0eb888e3fd25a18159
this is tea fr🫖 <164060354+tea-user@users.noreply.github.com>
2024-04-02 22:21:56 +0400

    Add files via upload

---
+++ Frontend/day_51/homework/hw_1/assignment/script.js
@@ -0,0 +1,18 @@
+/*
+    დავალება:
+    შექმენით ცვლადი (გამოიყინეთ ორივე მეშვეობა(keyword) და მათი თვისებები ) ამ ცვლადებში შემოიტანეთ თქვენი ოჯახის წევრების სახელები, გვარები და წლოვანებები და აგრეთვე რამდენი წლის იქნება თითოეული მათგანი 20 წელიწადში
+*/
+
+let name_1 = 'alex ghaduashvili'
+let name_2 = 'tamuna gongadze'
+
+const age_1 = 42
+const age_2 = 39
+
+console.log(name_1 + ' ' + age_1);
+console.log(name_2 + ' ' + age_2);
+
+console.log('After 20 years')
+
+console.log(name_1 + ' ' + Number(age_1 + 20));
+console.log(name_2 + ' '+ Number(age_2 + 20));
